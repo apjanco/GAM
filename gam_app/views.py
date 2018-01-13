494132a3127b06193129bf3e8a0ddf0e3b8ffa88
apjanco <ajanco@haverford.edu>
2018-01-13 00:42:43 -0500

    found simpler way to handle pk in views archivo__nombre_de_archivo=archivo

--- gam_app/views.py
+++ gam_app/views.py
@@ -3,7 +3,6 @@ from .models import *
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from gam_app import advanced_search
-from gam_app.utils import get_archivo_id, get_colección_id
 
 
 # Create your views here.
@@ -82,65 +81,46 @@ def advanced_search_submit(request):
         context = {"failed" : True}
         return render(request, 'index.html', context)
 
-#TODO: this section works, but should be revised by someone with a more-than-basic understanding of Django
-#documento5 is a hack that only works with the desapareccidos collection and GAM archive
-#using a simple get() raises: 'The QuerySet value for an exact lookup must be limited to one result using slicing.'
-#No help on SO or elsewhere (APJ 1/10/18)
+
 def documento5(request, archivo, colección, caja, legajo, carpeta, número_de_imagen):
-	archivo5_id = get_archivo_id(archivo)
-	colección5_id = get_colección_id(colección)
 	
-	state = Imagen.objects.filter(archivo=archivo5_id, colección=colección5_id, caja=caja, legajo=legajo, carpeta=carpeta, número_de_imagen=número_de_imagen)	
+	state = Imagen.objects.filter(archivo__nombre_del_archivo=archivo, colección__nombre_de_la_colección=colección, caja=caja, legajo=legajo, carpeta=carpeta, número_de_imagen=número_de_imagen)	
 	location = {'archivo':archivo, 'colección':colección, 'caja':caja, 'legajo':legajo, 'carpeta':carpeta, 'número_de_imagen':número_de_imagen}
 	context = {'state':state, 'location':location}
 	return render(request, 'all_documents_page.html', context)
 
 def documento4(request, archivo, colección, caja, legajo, carpeta):
 
-	archivo4_id = get_archivo_id(archivo)
-	colección4_id = get_colección_id(colección)
-
-	state = Imagen.objects.filter(archivo=archivo4_id, colección=colección4_id, caja=caja, legajo=legajo, carpeta=carpeta)	
+	state = Imagen.objects.filter(archivo__nombre_del_archivo=archivo, colección__nombre_de_la_colección=colección, caja=caja, legajo=legajo, carpeta=carpeta)	
 	location = {'archivo':archivo, 'colección':colección, 'caja':caja, 'legajo':legajo, 'carpeta':carpeta}
 	context = {'state':state, 'location':location}
 	return render(request, 'all_documents_page.html', context)
 
 def documento3(request, archivo, colección, caja, legajo):
 	
-	archivo3_id = get_archivo_id(archivo)
-	colección3_id = get_colección_id(colección)
-
-
-	state = Imagen.objects.filter(archivo=archivo3_id, colección=colección3_id, caja=caja, legajo=legajo)	
+	state = Imagen.objects.filter(archivo__nombre_del_archivo=archivo, colección__nombre_de_la_colección=colección, caja=caja, legajo=legajo)	
 	location = {'archivo':archivo, 'colección':colección, 'caja':caja, 'legajo':legajo}
 	context = {'state':state, 'location':location}
 	return render(request, 'all_documents_page.html', context)
 
 def documento2(request, archivo, colección, caja):
 	
-	archivo2_id = get_archivo_id(archivo)
-	colección2_id = get_colección_id(colección)
-
-	state = Imagen.objects.filter(archivo=archivo2_id, colección=colección2_id, caja=caja)	
+	state = Imagen.objects.filter(archivo__nombre_del_archivo=archivo, colección__nombre_de_la_colección=colección, caja=caja)	
 	location = {'archivo':archivo, 'colección':colección, 'caja':caja}
 	context = {'state':state, 'location':location}
 	return render(request, 'all_documents_page.html', context)
 
 def documento1(request, archivo, colección):
 	
-	archivo1_id = get_archivo_id(archivo)
-	colección1_id = get_colección_id(colección)
 
-	state = Imagen.objects.filter(archivo=archivo1_id, colección=colección1_id)	
+	state = Imagen.objects.filter(archivo__nombre_del_archivo=archivo, colección__nombre_de_la_colección=colección)	
 	location = {'archivo':archivo, 'colección':colección}
 	context = {'state':state, 'location':location}
 	return render(request, 'all_documents_page.html', context)
 
 def documento0(request, archivo):
 	
-	archivo_id = get_archivo_id(archivo)
-
-	state = Imagen.objects.filter(archivo=archivo_id)
+	state = Imagen.objects.filter(archivo__nombre_del_archivo=archivo)
 	location = {'archivo':archivo}
 	context = {'state':state,'location':location}
 	return render(request, 'all_documents_page.html', context)

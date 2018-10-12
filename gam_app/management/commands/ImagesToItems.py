0a59add72da7b0fec551d30a2c4b5dece2161727
root <apjanco@gmail.com>
2018-10-12 17:39:31 +0000

    ImagesToItems now ignores bag names with spaces

--- gam_app/management/commands/ImagesToItems.py
+++ gam_app/management/commands/ImagesToItems.py
@@ -23,39 +23,41 @@ class command(BaseCommand):
 
         #Variable to keep track of filenames
         for bag in bagValues:
-            queryImagens = Imagens.objects.filter(bag_names = bag).order_by("número_de_imagen")
-            ### Is this query in order. Are the images in the order listed like in the example below???
-            #The order_by should handle this, but I am unser.
-            
-            #Code adapted from import_dip.py
-            lastKnownFileName = ""
-            for image in queryImagens:
-                #Splitting the image filename. The image filename is similar to the item name. 
-                #From taa1cf992-b86e-4eab-bcb2-5b45958hat, we can call the items by filename.
-                #If it fails, then its item name is the one above it that passed.
-                letters = ''.join([i for i in image.localizacion_fisica[8:] if not i.isdigit()])
-                imageFile = image.localizacion_fisica[:-len(letters)]
-                #The letters takes into account image files with endings with multiple letters.
+            if bag is not Null:  #  This is needed because some bag names are not true bags.  They reflect the name as we received it from Guatemala with spaces.
+                if ' ' not in bag:
+                    queryImagens = Imagens.objects.filter(bag_names = bag).order_by("número_de_imagen")
+                    ### Is this query in order. Are the images in the order listed like in the example below???
+                    #The order_by should handle this, but I am unser.
+                    
+                    #Code adapted from import_dip.py
+                    lastKnownFileName = ""
+                    for image in queryImagens:
+                        #Splitting the image filename. The image filename is similar to the item name. 
+                        #From taa1cf992-b86e-4eab-bcb2-5b45958hat, we can call the items by filename.
+                        #If it fails, then its item name is the one above it that passed.
+                        letters = ''.join([i for i in image.localizacion_fisica[8:] if not i.isdigit()])
+                        imageFile = image.localizacion_fisica[:-len(letters)]
+                        #The letters takes into account image files with endings with multiple letters.
 
 
 
-                #Retrieving the item from our information above
-                try:
-                    myItem = Item.objects.get(nombre_del_item = imageFile)
-                    #If there is no item with that filename, it will raise an error.
-                    image.item =  imageFile
-                    lastKnownFileName = imageFile
-                except:
-                    '''
-                    image=  gam_des_001_001_004_001a  item = gam_des_001_001_004_001
-                            gam_des_001_001_004_002a         "
-                            gam_des_001_001_004_003          gam_des_001_001_004_003
-                            gam_des_001_001_004_004b         gam_des_001_001_004_004
-                            gam_des_001_001_004_005b         "
-                            gam_des_001_001_004_006b         "
-                    '''
-                    #Looking at this example, the second image would fail to find an item with its name
-                    #It would instead look at the last known image that succeeded.
-                    #It would then set its item information to that last known item, its 'host'. 
-                    image.item = lastKnownFileName
-                    pass
+                        #Retrieving the item from our information above
+                        try:
+                            myItem = Item.objects.get(nombre_del_item = imageFile)
+                            #If there is no item with that filename, it will raise an error.
+                            image.item =  imageFile
+                            lastKnownFileName = imageFile
+                        except:
+                            '''
+                            image=  gam_des_001_001_004_001a  item = gam_des_001_001_004_001
+                                    gam_des_001_001_004_002a         "
+                                    gam_des_001_001_004_003          gam_des_001_001_004_003
+                                    gam_des_001_001_004_004b         gam_des_001_001_004_004
+                                    gam_des_001_001_004_005b         "
+                                    gam_des_001_001_004_006b         "
+                            '''
+                            #Looking at this example, the second image would fail to find an item with its name
+                            #It would instead look at the last known image that succeeded.
+                            #It would then set its item information to that last known item, its 'host'. 
+                            image.item = lastKnownFileName
+                            pass

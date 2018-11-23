13422c41f09160c1ac3f75747cc43d4e83a39fbd
root <apjanco@gmail.com>
2018-11-23 18:58:04 +0000

    updates to index from Luis, models changes to address error viewing carpetas in admin

--- gam_app/models.py
+++ gam_app/models.py
@@ -140,12 +140,15 @@ class Carpeta(models.Model):
     tipo_de_violencia= models.CharField(max_length=200, blank=True)
     descripción = RichTextField(blank=True, default='')
     descripción_generada_automaticamente = RichTextField(blank=True)
-    person_status = models.CharField(max_length= 20, choices=PERSON_STATUS_CHOICES, null=True)
-    place_status = models.CharField(max_length= 20, choices=PLACE_STATUS_CHOICES, null=True)
-    organization_status = models.CharField(max_length= 20, choices=ORGANIZATION_STATUS_CHOICES, null=True)
+    person_status = models.CharField(max_length= 20, choices=PERSON_STATUS_CHOICES, null=True, blank=True)
+    place_status = models.CharField(max_length= 20, choices=PLACE_STATUS_CHOICES, null=True, blank=True)
+    organization_status = models.CharField(max_length= 20, choices=ORGANIZATION_STATUS_CHOICES, null=True, blank=True)
 
     def __str__(self):
-        return '{}/{}/{}'.format(self.caja, self.legajo, self.carpeta)
+        if '{}/{}/{}'.format(self.caja, self.legajo, self.carpeta) is None:
+            return "ERROR"
+        else:
+            return '{}/{}/{}'.format(self.caja, self.legajo, self.carpeta)
     
 
 #This is an archival collection

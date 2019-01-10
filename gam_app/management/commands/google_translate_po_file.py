b523618f57c6c68f3e18d92a6ce1ccddd10edaf2
root <apjanco@gmail.com>
2019-01-10 20:36:53 +0000

    small fixes to po translator

--- gam_app/management/commands/google_translate_po_file.py
+++ gam_app/management/commands/google_translate_po_file.py
@@ -1,16 +1,13 @@
 import re
 import sys
 from django.core.management.base import BaseCommand, CommandError
-from gam_app.models import *
-import googleapiclient.discovery
-from google.cloud import translate
-from gam_app.settings_secret import API_KEY
+from archivo.settings_secret import API_KEY
 from googleapiclient.discovery import build
 
 
 
 class Command(BaseCommand):
-    help = "Uses Google Translate to automatically populate the Django po file with translations. Takes arguments for source language, target language and file path. Note: English = en, Spanish = es, German = de."
+    help = "Uses Google Translate to automatically populate the Django po file with translations. Don't forget to makemessages to update the po file first. Ex. '$ python manage.py google_translate_po_file es de /home/ajanco/GAM/gam_app/locale/de/LC_MESSAGES/django.po'  Takes arguments for source language, target language and file path. Note: English = en, Spanish = es, German = de."
 
     def add_arguments(self, parser):
         parser.add_argument('source', nargs='+', type=str, help='The language of the source text, set to one of the language codes listed in Language Support. If the source language is not specified, the API will attempt to detect the source language automatically and return it within the response.')
@@ -18,6 +15,12 @@ class Command(BaseCommand):
         parser.add_argument('filename', nargs='+', type=str, help='The path and filename of the django.po file you want to update.')
 
     def handle(self, *args, **options):
+        from archivo.settings_secret import API_KEY
+
+        source = options['source'][0]
+        target = options['target'][0]
+        filename = options['filename'][0]
+
         def translate(source, target, q):
             return service.translations().list(source=source, target=target, q=[q]).execute()['translations'][0][
                 'translatedText']
@@ -40,6 +43,6 @@ class Command(BaseCommand):
             return content[:begin] + ('msgid "%s"\nmsgstr "%s"\n#. Autotranslated' % (
             source_text, translate(source, target, source_text))) + create_translations(source, target, content[end:])
 
-        service = build('translate', 'v2', developerKey=APIKEY)
+        service = build('translate', 'v2', developerKey=API_KEY)
         fill_django_po(source, target, filename)
 
